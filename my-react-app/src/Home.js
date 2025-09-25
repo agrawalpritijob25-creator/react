46be5f23717585b98fb13dd8e8e022aa043aa370
Priti Agrawal <agrawalpritijob25@gmail.com>
2025-09-25 13:56:02 +0530

    initial setup

---
+++ my-react-app/src/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "./logo.svg";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div class="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Home Page</h1>
+      <button onClick={() => navigate("/about")} className="aboutBtn">
+        Go to About Page
+      </button>
+    </div>
+  );
+}
+
+export default Home;
